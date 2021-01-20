d7a504e4a079c7f8b2519e27035fab6835ec7357
RebeccaKristanto <rebeccadinda16@gmail.com>
2021-01-20 20:20:25 +0700

    seccond commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{ 
 	public static void main(String[] args){ 
-	System.out.println("RebeccaKristanto: Aplikasi Java"); 
+	System.out.println("RebeccaKristanto: Aplikasi Java 2020"); 
 	}
 }	
